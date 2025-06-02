7a12caab55a87f876396d359a6bf0c77e4a5c121
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-06-02 21:58:09 +0200

    ResizeUtil base impl, no resizing yet

---
+++ src/main/java/dev/tizu/boykisserutils/ResizeUtil.java
@@ -0,0 +1,89 @@
+package dev.tizu.boykisserutils;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+public class ResizeUtil {
+    public static final LiteralArgumentBuilder<CommandSourceStack> COMMAND = Commands.literal("height")
+            .then(Commands.argument("size", StringArgumentType.greedyString()).executes(context -> {
+                var sender = context.getSource().getSender();
+                var sizestr = context.getArgument("size", String.class);
+
+                float height;
+                try {
+                    height = getParsedHeight(sizestr);
+                } catch (IllegalStateException e) {
+                    sender.sendMessage(Component.text(e.getMessage(), NamedTextColor.RED));
+                    return 0;
+                }
+
+                var convheight = height / 1.8f;
+                sender.sendActionBar(Component.text("Resized to " + height + "m, aka " + convheight + "st"));
+
+                return Command.SINGLE_SUCCESS;
+            }));
+
+    // default minecraft player height reference: 180cm, 1bl = 1m = 16px
+    // Map<unit, 1m in unit>
+    private static final Map<String, Float> HEIGHTMAP = Map.ofEntries(
+            // in-game
+            Map.entry("px", 16.0f),
+            Map.entry("steves", 1.8f),
+            Map.entry("%", 1.8f / 100.0f),
+            // real-world
+            Map.entry("cm", 0.01f),
+            Map.entry("m", 1.0f),
+            Map.entry("in", 0.0254f),
+            Map.entry("inch", 0.0254f),
+            Map.entry("ft", 0.3048f),
+            Map.entry("foot", 0.3048f),
+            Map.entry("yd", 0.9144f),
+            Map.entry("yard", 0.9144f),
+            // people
+            Map.entry("nico", 1.65f),
+            Map.entry("tizu", 1.87f),
+            // joke
+            Map.entry("cat", 0.24f),
+            Map.entry("lns", 0.3f),
+            Map.entry("catgirl", 1.62f));
+    private static final Pattern HEIGHT_REGEX = Pattern.compile(
+            "^(\\d*\\.?\\d+)?\s*([%a-zA-Z]+)?$");
+
+    /**
+     * Parses a height string into a float.
+     * 
+     * @returns The parsed height in meters.
+     * @exception IllegalStateException If the height string is invalid.
+     * @credit ElNico56 for function name.
+     */
+    static float getParsedHeight(String s) {
+        var m = HEIGHT_REGEX.matcher(s);
+        if (!m.matches())
+            throw new IllegalStateException("Invalid height");
+
+        // default of 1.0 (100% player) in case of no number
+        var num = 1.0f;
+        if (m.group(1) != null)
+            num = Float.parseFloat(m.group(1));
+
+        // default to steves if no unit
+        var unit = m.group(2);
+        if (unit == null)
+            unit = "steves";
+        // tell the users the accepted units if they fucked up big time
+        if (HEIGHTMAP.get(unit) == null)
+            throw new IllegalStateException("Invalid unit, expected one of: " +
+                    String.join(", ", HEIGHTMAP.keySet()));
+
+        return num * HEIGHTMAP.get(unit);
+    }
+}
