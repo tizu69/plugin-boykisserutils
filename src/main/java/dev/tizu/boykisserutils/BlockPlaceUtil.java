ea7901f5115bd4b7bcecf858a72670c85bf96619
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-06-17 20:02:24 +0200

    add commandblock place util

---
+++ src/main/java/dev/tizu/boykisserutils/BlockPlaceUtil.java
@@ -0,0 +1,44 @@
+package dev.tizu.boykisserutils;
+
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Directional;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class BlockPlaceUtil implements Listener {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK ||
+                event.getItem().getType() != Material.COMMAND_BLOCK ||
+                event.getPlayer().getGameMode() != GameMode.CREATIVE ||
+                !event.getPlayer().hasPermission("boykisser.commandblock.place") ||
+                !event.getPlayer().hasPermission("minecraft.commandblock"))
+            return;
+
+        event.setCancelled(true);
+        var targetPos = event.getClickedBlock().getRelative(event.getBlockFace());
+        if (targetPos.getType() != Material.AIR)
+            return;
+        var targetFace = getFacingDirection(event.getPlayer()).getOppositeFace();
+
+        targetPos.setType(Material.COMMAND_BLOCK);
+        var directional = (Directional) targetPos.getBlockData();
+        directional.setFacing(targetFace);
+        targetPos.setBlockData(directional);
+    }
+
+    private static BlockFace getFacingDirection(Player player) {
+        float pitch = player.getLocation().getPitch();
+        if (pitch < -45)
+            return BlockFace.UP;
+        else if (pitch > 45)
+            return BlockFace.DOWN;
+        return player.getFacing();
+    }
+
+}
--- src/main/java/dev/tizu/boykisserutils/ThisPlugin.java
+++ src/main/java/dev/tizu/boykisserutils/ThisPlugin.java
@@ -7,9 +7,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 
 public class ThisPlugin extends JavaPlugin implements Listener {
+    public static ThisPlugin instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(new BlockPlaceUtil(), this);
         this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
             commands.registrar().register(ResizeUtil.COMMAND.build());
         });
