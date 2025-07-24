a41151f82dc64f384fdf8d67494a413c1a006ab4
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-07-24 21:27:49 +0200

    shiftplace: use ctrl instead of shift

--- src/main/java/dev/tizu/boykisserutils/tweaks/ShiftPlaceTweak.java
+++ src/main/java/dev/tizu/boykisserutils/tweaks/ShiftPlaceTweak.java
@@ -1,15 +1,22 @@
 package dev.tizu.boykisserutils.tweaks;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.Rotatable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInputEvent;
 
 public class ShiftPlaceTweak implements Listener {
+	private static Set<UUID> controlling = new HashSet<>();
+
 	@EventHandler
 	public static void onBlockPlace(BlockPlaceEvent event) {
-		if (!event.getPlayer().isSneaking())
+		if (!controlling.contains(event.getPlayer().getUniqueId()))
 			return;
 		var bs = event.getBlockPlaced().getBlockData();
 		if (bs instanceof Directional dir)
@@ -17,5 +24,13 @@ public class ShiftPlaceTweak implements Listener {
 		else if (bs instanceof Rotatable rot)
 			rot.setRotation(rot.getRotation().getOppositeFace());
 		event.getBlockPlaced().setBlockData(bs);
+	}
+
+	@EventHandler
+	public static void onPlayerInput(PlayerInputEvent event) {
+		if (event.getInput().isSprint())
+			controlling.add(event.getPlayer().getUniqueId());
+		else
+			controlling.remove(event.getPlayer().getUniqueId());
 	}
 }
